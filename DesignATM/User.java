92abd59b7f949d3eed522848991cbf0ca16ff3d1
thug875d <yassirouaz@gmail.com>
2023-02-28 16:21:46 +0100

    this is my first atm impl

---
+++ DesignATM/User.java
@@ -0,0 +1,11 @@
+package DesignATM;
+
+public class User{
+    Card card;
+    UserBankAccount BankAccount;
+  public Card getCard() {
+      return card;
+  }public void setCard(Card card) {
+      this.card = card;
+  }
+}
